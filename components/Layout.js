d19b6b802373d6b00c80f0bea49ad7ff4513e2d6
Chetan-charan <chetanharicharan@gmail.com>
2022-01-16 22:23:59 +0530

    navbar userouter

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,20 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 
 const Layout = () => {
 
+    const router = useRouter();
+
     return <AppBar style={{marginBottom:'24px', paddingBottom: 0}} position="static">
     <Toolbar variant="dense">
-    <Button  variant="text" color='inherit'><Link href='/'>Home</Link></Button>
-    <Button  variant="text" color='inherit'><Link href='/furniture'>Furniture</Link></Button>
-    <Button  variant="text" color='inherit'><Link href='/appliances'>Appliances</Link></Button>
-    <Button  variant="text" color='inherit'><Link href='/fitness'>Fitness-funzone</Link></Button>
-    <Button  variant="text" color='inherit'><Link href='/laptops'>Laptops</Link></Button>
+    <Button onClick={() => {router.push('/')}} variant="text" color='inherit'>Home</Button>
+    <Button onClick={() => {router.push('/furniture')}} variant="text" color='inherit'>Furniture</Button>
+    <Button onClick={() => {router.push('/appliances')}} variant="text" color='inherit'>Appliances</Button>
+    <Button onClick={() => {router.push('/fitness')}} variant="text" color='inherit'>Fitness-funzone</Button>
+    <Button onClick={() => {router.push('/laptops')}}  variant="text" color='inherit'>Laptops</Button>
     </Toolbar>
   </AppBar>
 
